e9cca13e392052bb6074a76d60094ebc3c0e84e5
Md. Mohaimenul Reza <42312488+rezasoftbd@users.noreply.github.com>
2023-07-10 16:41:54 +0600

    Update color.dart

--- lib/extra/color.dart
+++ lib/extra/color.dart
@@ -9,9 +9,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
-  static const Color defaultDateColor = Colors.black;
-  static const Color defaultDayColor = Colors.black;
+  static const Color defaultDateColor = Colors.grey;
+  static const Color defaultDayColor = Colors.grey;
   static const Color defaultMonthColor = Colors.black;
-  static const Color defaultSelectionColor = Color(0x30000000);
+  static const Color defaultSelectionColor = Colors.grey;
   static const Color defaultDeactivatedColor = Color(0xFF666666);
 }
