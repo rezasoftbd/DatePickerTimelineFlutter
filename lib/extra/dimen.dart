43f616d3c868c65a398443df30b8fa26943bb864
Md. Mohaimenul Reza <42312488+rezasoftbd@users.noreply.github.com>
2023-07-10 16:42:15 +0600

    Update dimen.dart

--- lib/extra/dimen.dart
+++ lib/extra/dimen.dart
@@ -8,7 +8,7 @@
 class Dimen {
   Dimen._();
 
-  static const double dateTextSize = 24;
-  static const double dayTextSize = 11;
+  static const double dateTextSize = 13;
+  static const double dayTextSize = 13;
   static const double monthTextSize = 11;
 }
